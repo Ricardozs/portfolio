adbacc26759070b82a393b1087a3139d49bfe21c
Ricardo Sanchez <ricardo.sanchez@vhb.com>
2020-06-02 15:45:48 -0500

    deploy to firebase and benefits complete

--- src/app/modules/about/presentation/presentation.component.ts
+++ src/app/modules/about/presentation/presentation.component.ts
@@ -13,19 +13,33 @@ import { transition, trigger, state, animate, style } from '@angular/animations'
         opacity: 0,
       })),
       state('show', style({
-        transform: 'translateX(-0)',
+        transform: 'translateX(0)',
         opacity: 1,
       })),
       transition('hide => show', [
         animate('1s')
       ]),
     ]),
+    trigger('disappearAppaer', [
+      state('appear', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      state('disappear', style({
+        opacity:0,
+        transform: 'translateY(-10px)'
+      })),
+      transition('disappear => appear', [
+        animate('1s')
+      ])
+    ])
   ],
 })
 export class PresentationComponent implements OnInit {
   window;
   @HostListener('window:scroll', ['$event']) onScrollEvent($event){
     this.window = window.pageYOffset;
+    console.log(this.window)
   } 
   constructor() { }
 
