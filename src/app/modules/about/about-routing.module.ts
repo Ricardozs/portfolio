054fe4fa3dc55639f870422bb50b7af7afb5ca83
Ricardo Sanchez <ricardo.sanchez@vhb.com>
2020-06-05 07:30:31 -0500

    start component who we are

--- src/app/modules/about/about-routing.module.ts
+++ src/app/modules/about/about-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WhoWeAreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/about/about.module.ts
+++ src/app/modules/about/about.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WhoWeAreComponent],
   imports: [
     CommonModule,
     AboutRoutingModule
